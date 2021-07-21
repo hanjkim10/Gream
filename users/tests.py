bad4286684c4e88f9b3d9bdd6e9b4d6c0f0988b1
anjaekk <anjaekk@gmail.com>
2021-07-21 21:45:07 +0900

    Add: Signup view
    Add: Signup unit test

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,169 @@
-from django.test import TestCase
+import json
+import bcrypt
 
-# Create your tests here.
+from django.test  import TestCase, Client
+
+from users.models import User
+
+class SignupTest(TestCase):
+    def setUp(self):
+        User.objects.create(
+            id           = 1,
+            email        = 'kimcode@gmail.com',
+            password     = '1234@yyyy',
+            phone_number = '01090908080',
+            name         = '김코드'
+        )
+
+    def tearDown(self):
+        User.objects.all().delete()
+
+    def test_signupview_post_success(self):
+        client = Client()
+        user= {
+            'email'       :'leecode@gmail.com',
+            'password'    :'1234@yyyy',
+            'phone_number':'01090901111',
+            'name'        :'이코드'
+        }
+        response = client.post('/users/signup', json.dumps(user), content_type='application/json')
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.json(), 
+            {
+                'message' : 'SUCCESS'
+            }
+        )
+
+    def test_signupview_post_invalid_email(self):
+        client = Client()
+        user= {
+            'email'       :'leecode',
+            'password'    :'1234@yyyy',
+            'phone_number':'01090901111',
+            'name'        :'이코드'
+        }
+        response = client.post('/users/signup', json.dumps(user), content_type='application/json')
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), 
+            {
+                'error':'INVALID_ERROR'
+            }
+        )
+
+    def test_signupview_post_invalid_password(self):
+        client = Client()
+        user= {
+            'email'       :'leecode',
+            'password'    :'1234',
+            'phone_number':'01090901111',
+            'name'        :'이코드'
+        }
+        response = client.post('/users/signup', json.dumps(user), content_type='application/json')
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), 
+            {
+                'error':'INVALID_ERROR'
+            }
+        )
+
+    def test_signupview_post_duplicated_email(self):
+        client = Client()
+        user= {
+            'email'       :'kimcode@gmail.com',
+            'password'    :'1234@yyyy',
+            'phone_number':'01090901111',
+            'name'        :'이코드'
+        }
+        response = client.post('/users/signup', json.dumps(user), content_type='application/json')
+
+        self.assertEqual(response.status_code, 409)
+        self.assertEqual(response.json(), 
+            {
+                'message':'DUPLICATE'
+            }
+        )
+
+    def test_signupview_post_duplicated_phone_number(self):
+        client = Client()
+        user= {
+            'email'       :'leecode@gmail.com',
+            'password'    :'1234@yyyy',
+            'phone_number':'01090908080',
+            'name'        :'이코드'
+        }
+        response = client.post('/users/signup', json.dumps(user), content_type='application/json')
+
+        self.assertEqual(response.status_code, 409)
+        self.assertEqual(response.json(), 
+            {
+                'message':'DUPLICATE'
+            }
+        )
+
+    def test_signupview_post_keyerror_email(self):
+        client = Client()
+        user= {
+            'password'    :'1234@yyyy',
+            'phone_number':'01090901111',
+            'name'        :'이코드'
+        }
+        response = client.post('/users/signup', json.dumps(user), content_type='application/json')
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), 
+            {
+                'message':'KEY_ERROR'
+            }
+        )
+
+    def test_signupview_post_keyerror_password(self):
+        client = Client()
+        user= {
+            'email'       :'leecode@gmail.com',
+            'phone_number':'01090901111',
+            'name'        :'이코드'
+        }
+        response = client.post('/users/signup', json.dumps(user), content_type='application/json')
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), 
+            {
+                'message':'KEY_ERROR'
+            }
+        )
+
+    def test_signupview_post_keyerror_phone_number(self):
+        client = Client()
+        user= {
+            'email'       :'leecode@gmail.com',
+            'password'    :'1234@yyyy',
+            'name'        :'이코드'
+        }
+        response = client.post('/users/signup', json.dumps(user), content_type='application/json')
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), 
+            {
+                'message':'KEY_ERROR'
+            }
+        )
+
+    def test_signupview_post_keyerror_name(self):
+        client = Client()
+        user= {
+            'email'       :'leecode@gmail.com',
+            'password'    :'1234@yyyy',
+            'phone_number':'01090901111',
+        }
+        response = client.post('/users/signup', json.dumps(user), content_type='application/json')
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), 
+            {
+                'message':'KEY_ERROR'
+            }
+        )
+
--- users/views.py
+++ users/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+import re, json, bcrypt, jwt
 
-# Create your views here.
+from django.views import View
+from django.http  import JsonResponse
+
+from users.models import User
+
+REGEX = {
+    'email'    : '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$',
+    'password' : '^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%#?&])[A-Za-z\d@$!%*#?&]{8,16}$'
+}
+
+class SignupView(View):
+    def post(self, request):
+        try:
+            data         = json.loads(request.body)
+            email        = data['email']
+            password     = data['password']
+            phone_number = data['phone_number']
+            name         = data['name']
+
+            if not re.match(REGEX['email'], email) or not re.match(REGEX['password'], password):
+                return JsonResponse({'error':'INVALID_ERROR'}, status=400)
+            
+            if User.objects.filter(email=email).exists() or User.objects.filter(phone_number=phone_number).exists():
+                return JsonResponse({'message':'DUPLICATE'},status=409)
+            
+            encoded_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+            User.objects.create(
+                email        = email,
+                password     = encoded_password.decode('utf-8'),
+                phone_number = phone_number,
+                name         = name
+            )
+            return JsonResponse({'message':'SUCCESS'}, status=201)
+        
+        except KeyError:
+            return JsonResponse({'message':'KEY_ERROR'},status=400)
