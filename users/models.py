2c3f78e59f8b6b7f52ee096c3376320b134a6aff
anjaekk <anjaekk@gmail.com>
2021-07-21 13:47:03 +0900

    Modify: model 오타수정

--- users/models.py
+++ users/models.py
@@ -6,7 +6,7 @@ class User(models.Model):
     phone_number = models.CharField(max_length=45, unique=True)
     kakao_id     = models.CharField(max_length=45, null=True)
     card_company = models.CharField(max_length=45, null=True)
-    card_numnber = models.CharField(max_length=45, null=True)
+    card_number  = models.CharField(max_length=45, null=True)
     address      = models.CharField(max_length=128, null=True)
 
     class Meta:
