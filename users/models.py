0a59644f19614c2d3d6b27f75f73e24901c07751
anjaekk <anjaekk@gmail.com>
2021-07-22 11:27:06 +0900

    Modify: user model 수정

--- users/models.py
+++ users/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 class User(models.Model):
     email        = models.EmailField(unique=True)
-    password     = models.CharField(max_length=256)
-    phone_number = models.CharField(max_length=45, unique=True)
+    password     = models.CharField(max_length=256, null=True)
+    phone_number = models.CharField(max_length=45, unique=True, null=True)
     kakao_id     = models.CharField(max_length=45, null=True)
+    name         = models.CharField(max_length=45, null=True)
     card_company = models.CharField(max_length=45, null=True)
     card_number  = models.CharField(max_length=45, null=True)
     bank_name    = models.CharField(max_length=45, null=True)
